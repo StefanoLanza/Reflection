9c2011a4b386637e1f1624c48e088da038e9315c
Stefano Lanza <typhoon3d@gmail.com>
2020-07-27 12:40:00 +0200

    Clean up tuple header

--- include/stdTupleType.h
+++ include/stdTupleType.h
@@ -9,39 +9,24 @@
 
 namespace Typhoon::Reflection::detail {
 
-template <class T>
-const Type* autoRegisterType(TypeDB& typeDB);
+template <class C, size_t index>
+Property tupleElementProperty(const char* name, TypeDB& typeDB, Allocator& allocator) {
+	using E = std::tuple_element_t<index, C>;
+	const Type* varType = autoRegisterType<E>(typeDB, allocator);
 
-template <class C>
-class TupleHelper {
-public:
-	template <size_t index>
-	static Property makeProperty(const char* name, TypeDB& typeDB, Allocator& allocator) {
-		using T = std::tuple_element_t<index, C>;
-		const Type* varType = autoRegisterType<T>(typeDB, allocator);
-		return { setter<index>(), getter<index>(), name, varType, Flags::all, Semantic::none };
-	}
+	auto getter = [](const void* self, void* temporary) {
+		const C* tuple = reinterpret_cast<const C*>(self);
+		*static_cast<E*>(temporary) = std::get<index>(*tuple);
+		return temporary;
+	};
 
-private:
-	template <size_t index>
-	static Getter getter() {
-		return [](const void* self, void* temporary) {
-			const C* tuple = reinterpret_cast<const C*>(self);
-			using T = std::tuple_element_t<index, C>;
-			*static_cast<T*>(temporary) = std::get<index>(*tuple);
-			return temporary;
-		};
-	}
+	auto setter = [](void* self, const void* value) {
+		C* tuple = reinterpret_cast<C*>(self);
+		std::get<index>(*tuple) = *static_cast<const E*>(value);
+	};
 
-	template <size_t index>
-	static Setter setter() {
-		return [](void* self, const void* value) {
-			C* tuple = reinterpret_cast<C*>(self);
-			using T = std::tuple_element_t<index, C>;
-			std::get<index>(*tuple) = *static_cast<const T*>(value);
-		};
-	}
-};
+	return { setter, getter, name, varType, Flags::all, Semantic::none };
+}
 
 template <typename Tuple, size_t ElementIndex>
 void registerTupleElement(StructType& tupleType, TypeDB& typeDB, Allocator& allocator) {
@@ -49,8 +34,8 @@ void registerTupleElement(StructType& tupleType, TypeDB& typeDB, Allocator& allo
 	static_assert(std::tuple_size_v<Tuple> <= std::size(elementName));
 
 	if constexpr (ElementIndex < std::tuple_size_v<Tuple>) {
-		tupleType.addProperty(TupleHelper<Tuple>::makeProperty<ElementIndex>(elementName[ElementIndex], typeDB, allocator));
-		registerTupleElement<Tuple, ElementIndex + 1>(tupleType, typeDB, allocator);
+		tupleType.addProperty(tupleElementProperty<Tuple, ElementIndex>(elementName[ElementIndex], typeDB, allocator));
+		registerTupleElement<Tuple, ElementIndex + 1>(tupleType, typeDB, allocator); // recurse
 	}
 }
 
@@ -63,6 +48,7 @@ struct autoRegisterHelper<std::tuple<Args...>> {
 		constexpr TypeId  typeId = getTypeId<Tuple>();
 		StructType* const tupleType =
 		    allocator.make<StructType>(typeId, sizeof(Tuple), alignof(Tuple), nullptr, buildMethodTable<Tuple>(), std::ref(allocator));
+		// Register all elements
 		registerTupleElement<Tuple, 0>(*tupleType, typeDB, allocator);
 
 		typeDB.registerType(tupleType);
